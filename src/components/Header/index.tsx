f721b21552f82c10d8275f913cc52f9d45dc0812
henricker <henriquevieira@alu.ufc.br>
2022-08-12 15:06:48 -0300

    feat: add Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,22 @@
+import { NavLink } from 'react-router-dom'
+import { HeaderContainer } from './style'
+
+import Logo from '../../assets/logo.svg'
+import { Scroll, Timer } from 'phosphor-react'
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <HeaderContainer>
+      <img src={Logo} alt="logotipo do ignite da rocketseat" />
+
+      <nav>
+        <NavLink to="/" title="timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
   )
 }
---
+++ src/components/Header/style.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+
+      &.active {
+        color: ${(props) => props.theme['green-500']};
+      }
+    }
+  }
+`
