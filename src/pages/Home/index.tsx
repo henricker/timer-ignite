4f6dcc0e45b864f99d502c8d03001797141d70d0
henricker <henriquevieira@alu.ufc.br>
2022-08-15 16:08:26 -0300

    feat: add countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,59 @@
+import { Play } from 'phosphor-react'
+import {
+  ButtonCountDownButton,
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  TaskInput,
+  TimeInput,
+} from './style'
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="project_name">Vou trabalhar em </label>
+          <TaskInput
+            id="project_name"
+            type="text"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
+          <label id="minutes">durante</label>
+          <TimeInput
+            id="minutes"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <ButtonCountDownButton type="submit">
+          <Play size={24} />
+          Começar
+        </ButtonCountDownButton>
+      </form>
+    </HomeContainer>
   )
 }
